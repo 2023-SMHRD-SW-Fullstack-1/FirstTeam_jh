1245f83aff361d7c3c217e43840a9c7601820de8
SMHRD <SMHRD@DESKTOP-L0MO97B>
2023-04-14 16:30:22 +0900

    팀원3 네번째 커밋

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -6,6 +6,8 @@ public class Ex01 {
 		System.out.println("팀장님 첫번째 커밋");
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀원2 세번째 커밋");
+		System.out.println("팀원3(28) 세번째 커밋");
 	}
+	
 
 }
