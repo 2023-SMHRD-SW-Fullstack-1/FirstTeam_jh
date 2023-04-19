31106cef4e418e9a2500417f1aa9db0e855488a3
smhrd <smhrd@DESKTOP-S9FBFJ7>
2023-04-19 10:52:39 +0900

    User_Dummy

---
+++ Java_MiniProject/src/UserDao_Dummy.java
@@ -0,0 +1,32 @@
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UserDao_Dummy {
+	
+	private String InputId;
+	private String InputPw;
+	private String nick;
+	
+	//DB연결
+	public void getConn() {
+		
+	}
+	
+	//DB연결 해제
+	public void close() {
+		
+	}
+	
+	//회원가입
+	public void sign() {
+		
+	}
+	
+	//로그인
+	public void login() {
+		
+	}
+
+}
